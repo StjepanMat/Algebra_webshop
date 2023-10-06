c8005279806ebce965fe422e66cf2383a8825227
StjepanMat <stjepan.mateljan@gmail.com>
2023-10-06 17:55:30 +0200

    Add project files.

---
+++ Movies/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200,MinimumLength = 2)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Active { get; set; } = true;
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual ICollection<ProductImage> ProductImages { get; set; }
+    }
+}
