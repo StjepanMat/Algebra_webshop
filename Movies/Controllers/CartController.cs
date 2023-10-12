47d2f0db3b704cc02bc5e8e3796fb78b23eed2ca
StjepanMat <stjepan.mateljan@gmail.com>
2023-10-12 14:17:27 +0200

    Cart #1: made Extensions, made CartController

---
+++ Movies/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Data;
+using Movies.Extensions;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class CartController : Controller
+    {
+        public const string SessionKeyName = "_cart";
+        
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<CartItem> cart=HttpContext.Session.GetObjectFromJson<List<CartItem>>(SessionKeyName);
+            if (cart == null ) cart = new List<CartItem>();
+            decimal total = 0;
+            foreach (CartItem item in cart)
+            {
+                total += item.getTotal();
+            }
+            ViewBag.CartTotal = total;
+            return View();
+        }
+    }
+}
