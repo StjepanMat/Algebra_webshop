d6662e5adfe7a2fd1011f6f1791b7586c1465608
StjepanMat <stjepan.mateljan@gmail.com>
2023-10-16 16:58:44 +0200

    Order#01 HomeController Order action added, View Home/Order created

--- Movies/Controllers/HomeController.cs
+++ Movies/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movies.Data;
+using Movies.Extensions;
 using Movies.Models;
 using System.Diagnostics;
 
@@ -7,6 +8,8 @@ namespace Movies.Controllers
 {
     public class HomeController : Controller
     {
+        public const string SessionKeyName = "_cart";
+
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
 
@@ -29,7 +32,7 @@ namespace Movies.Controllers
 
         public IActionResult Product(int? id, int? categoryID)
         {
-            List<Product> products = _context.Product.ToList();
+            List<Product> products = _context.Product.Where(x=>x.Active==true).ToList();
 
             if (id != null)
             {
@@ -52,6 +55,54 @@ namespace Movies.Controllers
             
             ViewBag.Categories = _context.Category.ToList();
             return View(products);
+        }
+
+        public IActionResult Order(List<string> errors)
+        {
+            List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>(SessionKeyName);
+            if (cart == null)
+            {
+                return RedirectToAction("Index");
+            }
+            if (cart.Count == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            for(int i = 0; i<cart.Count;i++)
+            {
+                var product = _context.Product.Find(cart[i].Product.Id);
+                if (product == null)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product not found and was removed from cart!");
+                    continue;
+                }
+                if (product.Quantity < cart[i].Quantity)
+                {
+                    cart[i].Quantity = product.Quantity;
+                    errors.Add("Product quantity was reduced to available quantity!");
+                }
+                if(!product.Active)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product is not active and was removed from cart!");
+                    continue;
+                }
+            }
+            HttpContext.Session.SetObjectAsJson(SessionKeyName, cart);
+
+            foreach(CartItem item in cart)
+            {
+                item.Product.ProductImages=_context.ProductImage.Where(pi=>pi.ProductId==item.Product.Id).ToList();
+                item.Product.ProductCategories = _context.ProductCategory.Where(pc => pc.ProductId == item.Product.Id).ToList();
+            }
+
+            ViewBag.Errors= errors;
+
+            return View(cart);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
